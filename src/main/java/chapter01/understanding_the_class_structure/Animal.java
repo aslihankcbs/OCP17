3bfaf95e240dbe8a45d190b98a2448854044e75f
aslihankcbs <kocabasasli66@gmail.com>
2023-09-28 23:03:57 +0300

    Added Understanding the Class Structure.md

---
+++ src/main/java/chapter01/understanding_the_class_structure/Animal.java
@@ -0,0 +1,13 @@
+package chapter01.understanding_the_class_structure;
+
+public class Animal {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

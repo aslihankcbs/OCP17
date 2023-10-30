8fd9d6bf400997942b26e29b6fee80dbcf3c7e81
aslihankcbs <kocabasasli66@gmail.com>
2023-10-31 00:18:24 +0300

    Added Writing a main() Method

---
+++ src/main/java/chapter01/writing_a_main_method/ZooParamTest.java
@@ -0,0 +1,8 @@
+package chapter01.writing_a_main_method;
+
+public class ZooParamTest {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
